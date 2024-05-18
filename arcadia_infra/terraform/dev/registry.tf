15b79860d54f74215bb6a58a8f1133b92871ff6a
yugo_kamo <yugo.kamo@mythril.co.jp>
2024-05-18 18:24:06 +0900

    infra: web front用のArtifactRepository追加

--- arcadia_infra/terraform/dev/registry.tf
+++ arcadia_infra/terraform/dev/registry.tf
@@ -5,10 +5,25 @@ resource "google_artifact_registry_repository" "cloud-run-service-webapi" {
   format        = "DOCKER"
 }
 
-resource "google_artifact_registry_repository_iam_member" "iam-member-arcadia-dev-container-repository" {
+resource "google_artifact_registry_repository" "cloud-run-service-webfront" {
+  location      = var.location
+  repository_id = "cloud-run-service-webfront"
+  description   = "arcadia docker repository for webfront cloud run"
+  format        = "DOCKER"
+}
+
+resource "google_artifact_registry_repository_iam_member" "iam-member-arcadia-dev-webapi-container-repository" {
   provider = google-beta
   location  = var.location
   repository = google_artifact_registry_repository.cloud-run-service-webapi.name
+  role   = "roles/artifactregistry.admin"
+  member = "serviceAccount:${google_service_account.sa-github-actions.email}"
+}
+
+resource "google_artifact_registry_repository_iam_member" "iam-member-arcadia-dev-webfront-container-repository" {
+  provider = google-beta
+  location  = var.location
+  repository = google_artifact_registry_repository.cloud-run-service-webfront.name
   role   = "roles/artifactregistry.admin"
   member = "serviceAccount:${google_service_account.sa-github-actions.email}"
 }
